2f25cb65b818982a1031f5a2019fe1a9ed130f51
Adam D <adam820@gmail.com>
2014-03-28 02:08:55 -0400

    New vertical storage server, mini RAID. Changed export script to name as DPI instead of size.

--- export-pngs.sh
+++ export-pngs.sh
@@ -3,14 +3,14 @@
 for SVG in $(find ./scalable -type f);
 do
     FILENAME=${SVG##*/}
-    FILENAME=${FILENAME%.svg}_256.png
+    FILENAME=${FILENAME%.svg}_90dpi.png
     inkscape -d 90 -e ./png/$FILENAME $SVG
 done
 
 for SVG in $(find ./scalable -type f);
 do
     FILENAME=${SVG##*/}
-    FILENAME=${FILENAME%.svg}_512.png
+    FILENAME=${FILENAME%.svg}_180dpi.png
     inkscape -d 180 -e ./png/$FILENAME $SVG
 done
 
