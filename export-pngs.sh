53787df0b6b1b837c557f4829b75e4f53fa4a138
AdamD <adam820@gmail.com>
2017-09-21 16:06:00 -0400

    Update export script to export a single svg's png.

--- export-pngs.sh
+++ export-pngs.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-
-for SVG in $(find ./scalable -type f);
-do
-    FILENAME=${SVG##*/}
-    FILENAME=${FILENAME%.svg}_90dpi.png
-    inkscape -d 90 -e ./png/$FILENAME $SVG
-done
-
-for SVG in $(find ./scalable -type f);
-do
-    FILENAME=${SVG##*/}
-    FILENAME=${FILENAME%.svg}_180dpi.png
-    inkscape -d 180 -e ./png/$FILENAME $SVG
-done
-
+if [ ! $1 ]; then
+	for SVG in $(find ./scalable -type f);
+	do
+	    FILENAME=${SVG##*/}
+	    FILENAME=${FILENAME%.svg}_90dpi.png
+	    inkscape -d 90 -e ./png/$FILENAME $SVG
+	done
+	
+	for SVG in $(find ./scalable -type f);
+	do
+	    FILENAME=${SVG##*/}
+	    FILENAME=${FILENAME%.svg}_180dpi.png
+	    inkscape -d 180 -e ./png/$FILENAME $SVG
+	done
+else
+	inkscape -d 90 -e ./png/"$1"_90dpi.png ./scalable/"$1".svg
+	inkscape -d 180 -e ./png/"$1"_180dpi.png ./scalable/"$1".svg
+fi
